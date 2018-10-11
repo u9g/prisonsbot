888c0c493768c71aa54591fdbee3d467b20229e6
Joehainess <39843347+Joehainess@users.noreply.github.com>
2018-10-11 15:53:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,8 +27,8 @@ bot.on("message", function(message) {
 
         case "a":
         if (message.author.id == "204248274826166272") {
-        var allchannels = bot.channels.get()
-        allchannels.send(message)
+        // var allchannels = bot.channels.get()
+        bot.channels.get().send(message)
         } break;
 
         case "prune":
